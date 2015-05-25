fbca7203b5c5041b255fcfb7ddd5c1fbc309d11f
Boris Stitnicky <borisstitnicky@centrum.cz>
2015-05-25 13:03:58 +0200

    rebuilding

--- lib/sy/matrix.rb
+++ lib/sy/matrix.rb
@@ -17,71 +17,71 @@ require 'matrix'
 # on the operation one wants to do with such matrices.
 #
 class Matrix
-  # # Matrix multiplication.
-  # #
-  # def * arg # arg is matrix or vector or number
-  #   case arg
-  #   when Numeric
-  #     rows = @rows.map { |row| row.map { |e| e * arg } }
-  #     return new_matrix rows, column_size
-  #   when Vector
-  #     arg = Matrix.column_vector arg
-  #     result = self * arg
-  #     return result.column 0
-  #   when Matrix
-  #     Matrix.Raise ErrDimensionMismatch if column_size != arg.row_size
-  #     if empty? then # if empty?, then reduce uses WILDCARD_ZERO
-  #       rows = Array.new row_size do |i|
-  #         Array.new arg.column_size do |j|
-  #           ( 0...column_size ).reduce WILDCARD_ZERO do |sum, c|
-  #             sum + arg[c, j] * self[i, c]
-  #           end
-  #         end
-  #       end
-  #     else             # if non-empty, reduce proceeds without WILDCARD_ZERO
-  #       rows = Array.new row_size do |i|
-  #         Array.new arg.column_size do |j|
-  #           ( 0...column_size ).map { |c| arg[c, j] * self[i, c] }.reduce :+
-  #         end
-  #       end
-  #     end
-  #     return new_matrix( rows, arg.column_size )
-  #   when SY::Magnitude # newly added - multiplication by a magnitude
-  #     # I am not happy with this explicit switch on SY::Magnitude type here.
-  #     # Perhaps coerce should handle this?
-  #     return map { |e| e * arg }
-  #   else
-  #     compat_1, compat_2 = arg.coerce self
-  #     return compat_1 * compat_2
-  #   end
-  # end
+  # Matrix multiplication.
+  #
+  def * arg # arg is matrix or vector or number
+    case arg
+    when Numeric
+      rows = @rows.map { |row| row.map { |e| e * arg } }
+      return new_matrix rows, column_size
+    when Vector
+      arg = Matrix.column_vector arg
+      result = self * arg
+      return result.column 0
+    when Matrix
+      Matrix.Raise ErrDimensionMismatch if column_size != arg.row_size
+      if empty? then # if empty?, then reduce uses WILDCARD_ZERO
+        rows = Array.new row_size do |i|
+          Array.new arg.column_size do |j|
+            ( 0...column_size ).reduce WILDCARD_ZERO do |sum, c|
+              sum + arg[c, j] * self[i, c]
+            end
+          end
+        end
+      else             # if non-empty, reduce proceeds without WILDCARD_ZERO
+        rows = Array.new row_size do |i|
+          Array.new arg.column_size do |j|
+            ( 0...column_size ).map { |c| arg[c, j] * self[i, c] }.reduce :+
+          end
+        end
+      end
+      return new_matrix( rows, arg.column_size )
+    when SY::Magnitude # newly added - multiplication by a magnitude
+      # I am not happy with this explicit switch on SY::Magnitude type here.
+      # Perhaps coerce should handle this?
+      return map { |e| e * arg }
+    else
+      compat_1, compat_2 = arg.coerce self
+      return compat_1 * compat_2
+    end
+  end
 
-  # #
-  # # Matrix division (multiplication by the inverse).
-  # #   Matrix[[7,6], [3,9]] / Matrix[[2,9], [3,1]]
-  # #     => -7  1
-  # #        -3 -6
-  # #
-  # def /(other)
-  #   case other
-  #   when Numeric
-  #     rows = @rows.collect {|row|
-  #       row.collect {|e| e / other }
-  #     }
-  #     return new_matrix rows, column_count
-  #   when Matrix
-  #     return self * other.inverse
-  #   when SY::Magnitude # newly added - multiplication by a magnitude
-  #     return self * ( 1 / other )
-  #   else
-  #     return apply_through_coercion(other, __method__)
-  #   end
-  # end
+  #
+  # Matrix division (multiplication by the inverse).
+  #   Matrix[[7,6], [3,9]] / Matrix[[2,9], [3,1]]
+  #     => -7  1
+  #        -3 -6
+  #
+  def /(other)
+    case other
+    when Numeric
+      rows = @rows.collect {|row|
+        row.collect {|e| e / other }
+      }
+      return new_matrix rows, column_count
+    when Matrix
+      return self * other.inverse
+    when SY::Magnitude # newly added - multiplication by a magnitude
+      return self * ( 1 / other )
+    else
+      return apply_through_coercion(other, __method__)
+    end
+  end
 
 
-  # # Creates a matrix of prescribed dimensions filled with wildcard zeros.
-  # # 
-  # def Matrix.wildcard_zero r_count, c_count=r_count
-  #   build r_count, c_count do |r, c| WILDCARD_ZERO end
-  # end
+  # Creates a matrix of prescribed dimensions filled with wildcard zeros.
+  # 
+  def Matrix.wildcard_zero r_count, c_count=r_count
+    build r_count, c_count do |r, c| WILDCARD_ZERO end
+  end
 end
--- lib/sy/version.rb
+++ lib/sy/version.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 module SY
   VERSION = "2.1.0"
   DEBUG = false # debug mode switch - sometimes there are lines like
